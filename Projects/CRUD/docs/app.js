ce7e6ab68bfc7bae32da621be923f6ed9f5a16b9
Monica Limas Sarasty <monicalimassarasty@Monicas-MacBook-Pro.local>
2022-10-25 01:39:22 -0500

    Borrando elementos

--- Projects/CRUD/docs/app.js
+++ Projects/CRUD/docs/app.js
@@ -1,10 +1,12 @@
 
-/**Funcion de capturar datos, almacenar y limpiar campos*/
+
 window.onload = function(){
+
+    /**Funcion de capturar datos, almacenar y limpiar campos*/
     var btnAgregar = document.getElementById('btnAgregar');
     btnAgregar.addEventListener('click',function(){
         
-        /**Captura de productos escrito en los inputs */
+        /**Captura de productos escritos en los inputs */
         var producto = document.getElementById("inputProducto").value;
         var cantidad = document.getElementById("inputCantidad").value;
 
@@ -25,16 +27,34 @@ window.onload = function(){
         document.getElementById("retCantidad").innerHTML = retCantidad;
     });
 
+    /**Editar elementos almacenados */
     var btnEditar = document.getElementById('btnEditar');
     btnEditar.addEventListener('click',function(){
+
         /**Obtener datos almacenados */
         var editProducto = localStorage.getItem("Producto");
         var editCantidad = localStorage.getItem("Cantidad");
 
-        /**Mostrar datos almacenados */
+        /**Mostrar datos almacenados y cambiar label del boton mientras se edita */
         document.getElementById("inputProducto").value = editProducto;
         document.getElementById("inputCantidad").value = editCantidad;
         document.getElementById("btnAgregar").innerHTML = "Editar";
-    
+
+    });
+
+    /**Eliminar elementos almacenados */
+    var btnEliminar = document.getElementById('btnBorrar');
+    btnEliminar.addEventListener('click',function(){
+        /**Obtener datos almacenados */
+        var elimProducto = localStorage.getItem("Producto");
+        var elimCantidad = localStorage.getItem("Cantidad");
+
+        /**Eliminar los valores */
+        elimProducto = localStorage.removeItem("Producto");
+        elimCantidad = localStorage.removeItem("Cantidad");
+
+        /**Limpiando los campos */
+        document.getElementById("retProducto").value = "";
+        document.getElementById("retCantidad").value = "";
     });
 };
