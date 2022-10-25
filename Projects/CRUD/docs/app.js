7bc2297196752df72240b73faaa69401fea259e9
Monica Limas Sarasty <monicalimassarasty@Monicas-MacBook-Pro.local>
2022-10-24 23:51:19 -0500

    Agregando logica javaScript

---
+++ Projects/CRUD/docs/app.js
@@ -0,0 +1,21 @@
+
+/**Funcion de capturar datos, almacenar y limpiar campos*/
+
+$(document).ready(function(){
+    $('#btnAgregar').click(function(){
+        
+        /**Captura de productos escrito en los inputs */
+        var producto = document.getElementById("inputProducto").value;
+        console.log('producto',producto);
+        var cantidad = document.getElementById("inputCantidad").value;
+        console.log('cantidad',cantidad);
+
+        /**Guardando los datos en localstorage */
+        localStorage.setItem("Producto", producto);
+        localStorage.setItem("Cantidad", cantidad);
+
+        /**Limpiando los campos o inputs */
+        document.getElementById("inputProducto").value = "";
+        document.getElementById("inputCantidad").value = "";
+    });
+});
